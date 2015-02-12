5adfe5ac6de47091649be45d7e8846735d9160b9
ensomag <ensomag@inf.upv.es>
2015-02-12 11:27:56 +0100

    Plantilla inicial

---
+++ build/main.d
@@ -0,0 +1,172 @@
+main.o: /home/nrikee/devkitPro/examples/nds/templates/arm9/source/main.c \
+ /home/nrikee/devkitPro/libnds/include/nds.h \
+ /home/nrikee/devkitPro/libnds/include/nds/libversion.h \
+ /home/nrikee/devkitPro/libnds/include/nds/ndstypes.h \
+ /home/nrikee/devkitPro/libnds/include/nds/bios.h \
+ /home/nrikee/devkitPro/libnds/include/nds/ndstypes.h \
+ /home/nrikee/devkitPro/libnds/include/nds/card.h \
+ /home/nrikee/devkitPro/libnds/include/nds/ndstypes.h \
+ /home/nrikee/devkitPro/libnds/include/nds/debug.h \
+ /home/nrikee/devkitPro/libnds/include/nds/dma.h \
+ /home/nrikee/devkitPro/libnds/include/nds/interrupts.h \
+ /home/nrikee/devkitPro/libnds/include/nds/ipc.h \
+ /home/nrikee/devkitPro/libnds/include/nds/memory.h \
+ /home/nrikee/devkitPro/libnds/include/nds/system.h \
+ /home/nrikee/devkitPro/libnds/include/nds/timers.h \
+ /home/nrikee/devkitPro/libnds/include/nds/fifocommon.h \
+ /home/nrikee/devkitPro/libnds/include/nds/touch.h \
+ /home/nrikee/devkitPro/libnds/include/nds/input.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/dynamicArray.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/linkedlist.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/background.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/video.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/sassert.h \
+ /home/nrikee/devkitPro/libnds/include/nds/memory.h \
+ /home/nrikee/devkitPro/libnds/include/nds/dma.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/boxtest.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/videoGL.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/cache.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/trig_lut.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/math.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/dynamicArray.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/cache.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/console.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/background.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/decompress.h \
+ /home/nrikee/devkitPro/libnds/include/nds/bios.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/exceptions.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/guitarGrip.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/image.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/pcx.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/input.h \
+ /home/nrikee/devkitPro/libnds/include/nds/touch.h \
+ /home/nrikee/devkitPro/libnds/include/nds/input.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/keyboard.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/math.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/paddle.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/pcx.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/piano.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/rumble.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/sassert.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/sound.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/sprite.h \
+ /home/nrikee/devkitPro/libnds/include/nds/system.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/window.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/sprite.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/trig_lut.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/video.h \
+ /home/nrikee/devkitPro/libnds/include/nds/arm9/videoGL.h
+
+/home/nrikee/devkitPro/libnds/include/nds.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/libversion.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/ndstypes.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/bios.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/ndstypes.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/card.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/ndstypes.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/debug.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/dma.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/interrupts.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/ipc.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/memory.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/system.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/timers.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/fifocommon.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/touch.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/input.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/dynamicArray.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/linkedlist.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/background.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/video.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/sassert.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/memory.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/dma.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/boxtest.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/videoGL.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/cache.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/trig_lut.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/math.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/dynamicArray.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/cache.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/console.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/background.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/decompress.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/bios.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/exceptions.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/guitarGrip.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/image.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/pcx.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/input.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/touch.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/input.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/keyboard.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/math.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/paddle.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/pcx.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/piano.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/rumble.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/sassert.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/sound.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/sprite.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/system.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/window.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/sprite.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/trig_lut.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/video.h:
+
+/home/nrikee/devkitPro/libnds/include/nds/arm9/videoGL.h:
